3fa77a63ae2bba15fc5829a1341b6da940822736
NhatQuang <165651273+nhquang2704@users.noreply.github.com>
2024-10-28 23:42:24 +0700

    up

--- demo_excercise/src/main/java/vn/iotstar/service/ICategoryService.java
+++ demo_excercise/src/main/java/vn/iotstar/service/ICategoryService.java
@@ -1,5 +1,41 @@
 package vn.iotstar.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import vn.iotstar.entity.CategoryEntity;
+
 public interface ICategoryService {
 
+	List<CategoryEntity> findByNameContaining(String name);
+
+	Page<CategoryEntity> findByNameContaining(String name, Pageable pageable);
+
+	<S extends CategoryEntity> S save(S entity);
+
+	<S extends CategoryEntity> Optional<S> findOne(Example<S> example);
+
+	List<CategoryEntity> findAll(Sort sort);
+
+	Page<CategoryEntity> findAll(Pageable pageable);
+
+	List<CategoryEntity> findAllById(Iterable<Long> ids);
+
+	Optional<CategoryEntity> findById(Long id);
+
+	long count();
+
+	void deleteById(Long id);
+
+	void delete(CategoryEntity entity);
+
+	void deleteAll();
+
+	List<CategoryEntity> findAll();
+	
 }
--- demo_excercise/src/main/java/vn/iotstar/service/Impl/CategoryServiceImpl.java
+++ demo_excercise/src/main/java/vn/iotstar/service/Impl/CategoryServiceImpl.java
@@ -1,5 +1,120 @@
 package vn.iotstar.service.Impl;
 
-public class CategoryServiceImpl {
+import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.data.domain.Sort;
+import java.util.Optional;
+import vn.iotstar.entity.CategoryEntity;
+import vn.iotstar.repository.CategoryRepository;
+import vn.iotstar.service.ICategoryService;
+
+
+@Service
+public class CategoryServiceImpl implements ICategoryService {
+
+	@Autowired
+	CategoryRepository categoryRepository;
+	
+	public CategoryServiceImpl(CategoryRepository categoryRepository) {
+		this.categoryRepository = categoryRepository;
+	}
+
+	
+	@Override
+	public List<CategoryEntity> findByNameContaining(String name) {
+		return categoryRepository.findByNameContaining(name);
+	}
+
+	
+	@Override
+	public Page<CategoryEntity> findByNameContaining(String name, Pageable pageable) {
+		return categoryRepository.findByNameContaining(name, pageable);
+	}
+
+	
+	@Override
+	public <S extends CategoryEntity> S save(S entity) {
+		if(entity.getCategoryId() == null) {
+			return categoryRepository.save(entity);
+		}else {
+			Optional<CategoryEntity> opt = findById(entity.getCategoryId());
+			if(opt.isPresent()) {
+				if(StringUtils.isEmpty(entity.getName())) {
+					entity.setName(opt.get().getName());
+				}else {
+					entity.setName(entity.getName());
+				}
+			}
+		return categoryRepository.save(entity);
+	}
+}
+
+	
+	@Override
+	public <S extends CategoryEntity> Optional<S> findOne(Example<S> example) {
+		return categoryRepository.findOne(example);
+	}
+
+	
+	@Override
+	public List<CategoryEntity> findAll(Sort sort) {
+		return categoryRepository.findAll(sort);
+	}
+
+	
+	@Override
+	public Page<CategoryEntity> findAll(Pageable pageable) {
+		return categoryRepository.findAll(pageable);
+	}
+
+	
+	@Override
+	public List<CategoryEntity> findAllById(Iterable<Long> ids) {
+		return categoryRepository.findAllById(ids);
+	}
+
+	
+	@Override
+	public Optional<CategoryEntity> findById(Long id) {
+		return categoryRepository.findById(id);
+	}
+
+	
+	@Override
+	public long count() {
+		return categoryRepository.count();
+	}
+
+	
+	@Override
+	public void deleteById(Long id) {
+		categoryRepository.deleteById(id);
+	}
+
+	
+	@Override
+	public void delete(CategoryEntity entity) {
+		categoryRepository.delete(entity);
+	}
+
+	
+	@Override
+	public void deleteAll() {
+		categoryRepository.deleteAll();
+	}
+
+
+	@Override
+	public List<CategoryEntity> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 }
